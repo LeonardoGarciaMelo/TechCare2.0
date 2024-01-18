412aae295731b7359f31362f1dcfe54cb65a5a81
LeonardoGarciaMelo <lgarciamelo213@gmail.com>
2024-01-18 11:31:30 -0300

    Created PatientRegisterData and CreatedAddressData

---
+++ TechCare/Models/Address/AddressData.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechCare.Models.Address
+{
+    public class AddressData
+    {
+        [Required(ErrorMessage = "City space is empty, please write something")]
+        public string City { get; set; } = null!;
+
+        [Required(ErrorMessage = "State space is empty, please write something")]
+        public string State { get; set; } = null!;
+
+        [Required(ErrorMessage = "PostalCode space is empty, please write something")]
+        [RegularExpression(@"^\d{8}$")]
+        public string PostalCode { get; set; } = null!;
+
+    }
+}
---
+++ TechCare/Models/Patient/PatientRegisterData.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TechCare.Models.Address;
+using TechCare.Models.Gender;
+
+namespace TechCare.Models.Patient
+{
+    public class PatientRegisterData
+    {
+        [Required(ErrorMessage = "Please, name space is empty, write something")]
+        [RegularExpression(@"^.+$")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please, age space is empty, write something")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Please, gender space is empty, write something")]
+        public EGenderModel Gender { get; set; }
+
+        [Required(ErrorMessage = "Please, phone space is empty, write something")]
+        public string Phone { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please, email space is empty, write something")]
+        [EmailAddress(ErrorMessage = "Email space pattern is wrong, please write a valid email")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please, address space is empty, write something")]
+        public AddressData Address { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please, diagnosis space is empty, write something")]
+        public string Diagnosis { get; set; } = null!;
+
+
+    }
+}
