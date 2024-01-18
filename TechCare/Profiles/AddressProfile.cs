295cd166d7874dc53e62072c0953ae04e3f65aff
LeonardoGarciaMelo <lgarciamelo213@gmail.com>
2024-01-18 11:37:32 -0300

    Mapping of PatientRegisterData and AddressData with their respective models

---
+++ TechCare/Profiles/AddressProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using TechCare.Models.Address;
+
+namespace TechCare.Profiles
+{
+    public class AddressProfile : Profile
+    {
+        public AddressProfile() 
+        {
+            CreateMap<AddressData, AddressModel>();
+        }
+    }
+}
---
+++ TechCare/Profiles/PatientProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using TechCare.Models.Patient;
+
+namespace TechCare.Profiles
+{
+    public class PatientProfile : Profile
+    {
+        public PatientProfile() 
+        {
+            CreateMap<PatientRegisterData, PatientModel>();
+        }
+    }
+}
